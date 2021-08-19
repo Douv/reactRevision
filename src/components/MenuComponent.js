ee0172a824a75ff4f088864ca6e34a97f4176dff
Wadly Dugue <dukyw1@gmail.com>
2021-08-19 10:52:59 -0300

    renderisar item con descripcion-comentarios-fechas

---
+++ src/components/MenuComponent.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import Dish from './DishDetailComponent';
+
+
+
+class Menu extends Component {
+    
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectedDish: null
+        }
+    }
+    
+    onDishSelect(dish) {
+        this.setState({ selectedDish: dish});
+    }
+
+    renderDish(dish) {
+        if (dish != null)
+            return(
+                <Card>
+                    <CardImg src={dish.image} alt={dish.name} width="100%" />
+                    <CardBody>
+                      <CardTitle>{dish.name}</CardTitle>
+                      <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            )
+        else
+            return(
+                <div></div>
+            )
+    }
+    render(){
+        
+        const menu = this.props.dishes.map((dish) => {
+            return (
+              <div key={dish.id} className="col-12 col-md-5 m-1">
+                <Card onClick={() => this.onDishSelect(dish)}>
+                  <CardImg src={dish.image} alt={dish.name} width="100%" />
+                  <CardImgOverlay>
+                    <CardTitle>{dish.name}</CardTitle>
+                  </CardImgOverlay>
+                </Card>
+              </div>
+            );
+        });
+        return(
+            <div className="container">
+                <div className="row">
+                    {menu}
+                </div>
+                <Dish dish={this.state.selectedDish} />
+            </div>
+        );
+    }
+}
+
+export default Menu;
